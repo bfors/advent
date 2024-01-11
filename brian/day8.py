bfaa15710244b20f765da5b9c3d4b83493598b1b
Brian Forsberg <bforsberg15@gmail.com>
2024-01-11 12:31:08 -0600

    Day 8 part 2

--- brian/day8.py
+++ brian/day8.py
@@ -1,12 +1,9 @@
 from AdventUtils import *
 import AdventUtils
-
+from math import lcm
 AdventUtils.current_year = 2023
 
-def haunted(input):
-    instructions = input[0]
-    nodes = input[2:]
-
+def get_dict(nodes):
     d = {}
     node: str
     for node in nodes:
@@ -14,6 +11,12 @@ def haunted(input):
         L = node[node.find("(") + 1 : node.find(",")]
         R = node[node.find(", ") + 2 : node.find(")")]
         d[element] = {"L": L, "R": R}
+    return d
+
+def haunted(input):
+    instructions = input[0]
+    nodes = input[2:]
+    d = get_dict(nodes)
 
     current = "AAA"
     steps = 0
@@ -26,5 +29,29 @@ def haunted(input):
 
 in8 = parse(8, show=0)
 print(answer(8.1, 16897, lambda: haunted(in8)))
+
+def haunted2(input):
+    instructions = input[0]
+    nodes = input[2:]
+    d = get_dict(nodes)
+
+    starters = [key for key in d.keys() if key[2] == 'A']
+    steparr = []
+    for starter in starters:
+        steps = 0
+        current = starter
+        while True:
+            if current[2] == "Z":
+                steparr.append(steps)
+                print(f'{starter}: {steps}')
+                break
+            circular_index = steps % len(instructions)
+            current = d[current][instructions[circular_index]]
+            steps += 1
+    return lcm(*steparr)
+
+
+print(answer(8.2, 16563603485021, lambda: haunted2(in8)))
+
 
 
