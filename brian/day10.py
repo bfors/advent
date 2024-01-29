5b61779347a84771ad65d4974c6f2ce0ea634511
Brian Forsberg <bforsberg15@gmail.com>
2024-01-28 22:37:34 -0600

    Solve day 10 part 1.

--- brian/day10.py
+++ brian/day10.py
@@ -10,23 +10,10 @@ def move(location, up=0, down=0, left=0, right=0):
 def get_coordinates(grid, loc):
     return [t for t in [move(loc, up=1), move(loc, down=1), move(loc,left=1), move(loc,right=1)] if t in grid]
 
-def get_next(grid, coordinates):
-
-
-
 def pipe(input):
-
-    # Load entire grid into a map
-    # Find start
-    # Pick valid direction
-    # while n is not S
-        # record path
-        # goto next valid direction
-
-    grid = defaultdict(dict)
+    grid = {}
     start = (-1, -1)
     for i, line in enumerate(input.splitlines()):
-        l = []
         for j, c in enumerate(line):
             grid[(i,j)] = c
             if c == 'S':
@@ -34,20 +21,49 @@ def pipe(input):
 
     path = [start]
     location = start
-    val = ''
 
-    valid_left = {'L', '-', 'F'}
-    valid_right = {'-', 'J', '7'}
-    valid_up = {'|', '7', 'F'}
-    valid_down = {'|', 'J', 'L'}
-
-    print(f'start: {start}')
     coordinates = get_coordinates(grid, location)
-    print(get_next(grid, coordinates))
+    location = move(location, up=1)
+    path.append(location)
 
+    while path[-1] != start:
+        next = get_adjacents(grid, path)
+        path.append(next)
+
+    s = set(path)
+    for i, line in enumerate(input.splitlines()):
+        l = []
+        for j, c in enumerate(line):
+            if (i,j) in s:
+                l.append('X')
+            else:
+                l.append(c)
+        print(''.join(l))
+
+    return len(path)//2
+        
+
+def get_adjacents(grid, path):
+    location = path[-1]
+    current = grid[location]
+    next = ()
+    if current == '|': 
+        next = (move(location, up=1), move(location, down=1))
+    elif current == 'L':
+        next = (move(location, up=1), move(location, right=1))
+    elif current == '-':
+        next = (move(location, left=1), move(location, right=1))
+    elif current == 'F':
+        next = (move(location, down=1), move(location, right=1))
+    elif current == '7':
+        next = (move(location, down=1), move(location, left=1))
+    elif current == 'J':
+        next = (move(location, left=1), move(location, up=1))
+
+    return next[1] if next[1] != path[-2] else next[0]
 
 
 filename = '../inputs/2023/input10.txt'
 input10 = pathlib.Path(filename).read_text()
 
-answer(10.1, 0, lambda: pipe(input10)) 
+print(answer(10.1, 6942, lambda: pipe(input10)))
