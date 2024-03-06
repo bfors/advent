696dbb1c7fc645ff1a8f4fcfd125a0da718828f9
Brian Forsberg <bforsberg15@gmail.com>
2024-03-06 10:57:27 -0600

    Add day 11 part 1 solution.

--- src/day11.py
+++ src/day11.py
@@ -1,33 +1,49 @@
 from AdventUtils import *
 import AdventUtils
-import polars as pl
 from itertools import combinations
 
 AdventUtils.current_year = 2023
-input11 = parse(12, parser=str.split, show=0)
+input = parse(11, parser=list, show=0)
 
 
 def cosmic(input):
-    y = 0
-    gx = list()
-    print(input[1])
+    gx = []
 
-    df = pl.DataFrame(input)
-    with pl.Config(fmt_str_lengths=2000, tbl_rows=200, tbl_cols=100):
-        print(df)
+    # Keep index of expanded space
+    y_expansion = []
+    x_expansion = []
+
+    # scan rows
+    y = 0
+    for line in input:
+        if line == len(line) * ["."]:
+            y += 1
+        y_expansion.append(y)
+        y += 1
+
+    # scan columns
+    x = 0
+    for i in range(len(input[0])):
+        col = [p[i] for p in input]
+        if col == len(col) * ["."]:
+            x += 1
+        x_expansion.append(x)
+        x += 1
+
+    for y, line in enumerate(input):
+        for x, c in enumerate(line):
+            if c == "#":
+                gx.append((x_expansion[x], y_expansion[y]))
 
     prod = combinations(gx, r=2)
-
     s = 0
     for p in prod:
-        print(p)
-        g1, g2 = p
-        g1x, g1y = g1
-        g2x, g2y = g2
+        galaxy1, galaxy2 = p
+        g1x, g1y = galaxy1
+        g2x, g2y = galaxy2
         dist = abs(g1x - g2x) + abs(g1y - g2y)
-        print(dist)
         s += dist
     return s
 
 
-print(answer(11.1, 0, lambda: cosmic(input11)))
+print(answer(11.1, 0, lambda: cosmic(input)))
--- src/test.py
+++ src/test.py
@@ -1,4 +1,5 @@
 from AdventUtils import *
+from collections import Counter
 import AdventUtils
 
 AdventUtils.current_year = 2023
@@ -8,16 +9,17 @@ from itertools import combinations
 input = parse(12, parser=list, show=0)
 df: pl.DataFrame = pl.DataFrame(input)
 
-with pl.Config(fmt_str_lengths=2000, tbl_rows=200, tbl_cols=100):
-    print(df)
-
 for i, c in enumerate(df.get_columns()):
-    series = pl.Series("", len(c) * ".")
-    with pl.Config(fmt_str_lengths=2000, tbl_rows=200, tbl_cols=100):
-        print(c)
-        print(series)
-    if c.equals(series):
-        print("we are equal")
-        s = pl.Series(len(c) * ".")
+    if len(c.value_counts().rows()) == 1:
+        s = pl.Series(str(i), len(c) * ".")
         df.insert_column(i, s)
-    print()
+
+gx = set()
+for row in df.iter_rows():
+    print(row)
+    if len(set(row)) == 1:
+        print(list(len(row) * "."))
+
+
+# with pl.Config(fmt_str_lengths=2000, tbl_rows=200, tbl_cols=100):
+#     print(df)
