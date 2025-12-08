4b4ede58de0bcd21a9ea59f25c371bcc2d0d44a3
bfors <bforsberg15@gmail.com>
2025-12-08 00:52:50 -0600

    Improve day 1 part 1.

--- src/2025/day1.py
+++ src/2025/day1.py
@@ -1,27 +1,19 @@
 with open("inputs/2025/input1.txt", "r") as f:
     num = 50
     zeros = 0
-    print("start")
-    print(num)
-    i = 0
+
     for line in f:
-        i += 1
         d = line[0]
         amt = int(line[1:-1])
 
         if d == "L":
-            num -= amt
-
-            if num < 0:
-                num %= -100
-                num = 100 + num
-
-        if d == "R":
-            num += amt
-            num %= 100
+            num = (num - amt) % 100
+        elif d == "R":
+            num = (num + amt) % 100
 
         if num == 0:
             zeros += 1
-        print(f"{d}:{amt}   num: {num}")
-    print(f"lines: {i}")
-    print(zeros)
+
+        print(f"{d}{amt} -> {num}")
+
+    print(f"\nPassword: {zeros}")
