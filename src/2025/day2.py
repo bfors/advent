dddf66775adb2fb1a7b5a83eccaa7cce5253463b
bfors <bforsberg15@gmail.com>
2025-12-13 21:57:47 -0600

    2025 Day 2 input.

--- src/2025/day2.py
+++ src/2025/day2.py
@@ -1,20 +1,3 @@
-import math
-
-dif = 0 + 150
-
-num = 0
-amt = 100
-orig = num
-dif = num - amt
-zs = int(math.fabs(math.floor(dif / 100)))
-
-num = (num - amt) % 100
-if num == 0:
-    zs += 1
-
-if orig == 0:
-    zs -= 1
-
-
-print(f"{orig} - {amt} -> {num}")
-print(zs)
+with open("inputs/2025/input1.txt", "r") as f:
+    for line in f:
+        print(line)
